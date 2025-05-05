f97e7d803e4662473884e478faf0bf7012637238
honghungluong-asus <luonghonghung95@gmail.com>
2025-05-05 10:52:14 +0700

    add health check

---
+++ Controllers/HeathCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaishenManagementAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HeathCheckController : ControllerBase
+    {
+        [HttpGet(Name = "HealthCheck")]
+        public string Get()
+        {
+            return "CaishenManagementAPI HealthCheck successfully!";
+        }
+    }
+}
