72a7c7b118b4f39fdbee778a478943c40f954b65
honghungluong-asus <luonghonghung95@gmail.com>
2025-05-06 20:20:45 +0700

    add migration addInPrice

---
+++ Migrations/20250506131724_addinPrice.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaishenManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addinPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "InPrice",
+                table: "Products",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InPrice",
+                table: "Products");
+        }
+    }
+}
